1f2f3d3063d51476d338d30009db30ea8468846a
Subhrajitbw <subhrajitbhowmik80@gmail.com>
2022-03-14 20:42:17 +0530

    Your awesome message

---
+++ src/Components/Buttons/Buttons.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Buttons() {
+  return (
+    <div>
+        <button className='float-1 btn btn-primary rounded-0 py-4 px-lg-5 d-lg-block'>E1</button>
+        <button className='float-1 btn btn-primary rounded-0 py-4 px-lg-5 d-lg-block'>E1</button>
+        <button className='float-1 btn btn-primary rounded-0 py-4 px-lg-5 d-lg-block'>E1</button>
+        <button className='float-1 btn btn-primary rounded-0 py-4 px-lg-5 d-lg-block'>E1</button>
+        <button className='float-1 btn btn-primary rounded-0 py-4 px-lg-5 d-lg-block'>E1</button>
+        <button className='float-1 btn btn-primary rounded-0 py-4 px-lg-5 d-lg-block'>E1</button>
+    </div>
+  )
+}
+
+export default Buttons
